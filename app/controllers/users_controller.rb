e9015aa2adea4c47e30a78bff0e37c2f5961ff34
ishihaya <ishidahayate49@gmail.com>
2019-10-07 15:47:15 +0900

    単体テスト成功

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,2 @@
 class UsersController < ApplicationController
-  
-  # def destroy
-  #   redirect_to controller: 'items', action: 'index'
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
+  # 仮
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
