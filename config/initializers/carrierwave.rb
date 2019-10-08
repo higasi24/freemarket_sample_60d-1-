fc695be97b1255d138deaf4e738bf244ec346341
higasi <higasi.nba1209@au.com>
2019-10-08 21:39:08 +0900

    carrierwave.rb
    change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
-  elsif Rails.env.production?
+  else Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -14,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1' #例 'ap-northeas-1'
   }
-
+  end
   config.fog_directory  = 'free1209'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/free1209'
 end
