27c545b39a814ab6e3ef051f21d95f57a74f119f
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-08 16:29:21 +0900

    ログアウト画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  get 'logout', to: 'mypages#show'
   resources :users, only: :show
   resources :items do
     get :search, on: :collection
