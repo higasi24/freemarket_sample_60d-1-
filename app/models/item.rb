141b9bdd67bd4a30c4243f97d8008c66a54db79d
ishihaya <ishidahayate49@gmail.com>
2019-10-08 14:54:33 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  belongs_to :area
+  belongs_to :prefecture
   belongs_to :brand
   has_many :images
   has_many :categories, through: :categories_items
