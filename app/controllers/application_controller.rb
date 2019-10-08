8742ef70be780f6018026bca1fb7656e04001271
higasi <higasi.nba1209@au.com>
2019-10-08 17:18:51 +0900

    h

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def production?
+    Rails.env.production?
+  end
+
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
