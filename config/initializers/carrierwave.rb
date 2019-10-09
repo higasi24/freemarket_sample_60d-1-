a0d841068ba03e03fbcd23b7713c3a358790c352
higasi <higasi.nba1209@au.com>
2019-10-09 16:56:54 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.secrets.aws[:secret_access_key],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1' #例 'ap-northeas-1'
   }
   end
