90ba8edebd05e1c09455c7e383d2acfd43d45b25
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-10-10 17:22:54 +0900

    modifyed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @images = Image.new
+    binding.pry
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.permit(:name, :detail, :state, :delivery_fee, :delivery_method, :price, :delivery_date, :area_id).merge( saler_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_061119) do
+ActiveRecord::Schema.define(version: 2019_10_10_073412) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "city", null: false
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 2019_10_08_061119) do
     t.datetime "updated_at", null: false
     t.bigint "area_id"
     t.bigint "brand_id"
+    t.integer "saler_id"
+    t.integer "buyer_id"
     t.index ["area_id"], name: "index_items_on_area_id"
     t.index ["brand_id"], name: "index_items_on_brand_id"
   end
