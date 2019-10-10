e71102146422792c5fd0700a5bb6a4cc74af5b53
ito0804takuya <ito0804takuya@gmail.com>
2019-10-10 17:47:19 +0900

    仮保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,11 @@
 class ItemsController < ApplicationController
+  # before_action :set_order, :set_address
+
   def index
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
@@ -10,4 +13,16 @@ class ItemsController < ApplicationController
 
   def edit
   end
+
+
+  # private
+
+  # def set_order
+  #   @order = Order.find(params[:saler_id])
+  # end
+
+  # def set_address
+  #   @address = Address.find(params[:saler_id])
+  # end
+
 end
