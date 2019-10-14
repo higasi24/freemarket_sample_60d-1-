ca3d70b3aa560f562a0e9e0945602c8efee120c0
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-14 11:35:16 +0900

    Userテーブルにカラム追加

--- Gemfile
+++ Gemfile
@@ -93,3 +93,5 @@ gem 'jquery-rails'
 gem 'gretel'
 gem 'payjp'
 # gem "omniauth-rails_csrf_protection"
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_13_075041) do
+ActiveRecord::Schema.define(version: 2019_10_14_023414) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "city", null: false
@@ -156,6 +156,8 @@ ActiveRecord::Schema.define(version: 2019_10_13_075041) do
     t.string "first_name_kana", null: false
     t.string "family_name_kana", null: false
     t.date "birth_date"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
