a168f1d53b9339edddf42429adc339d2fe823d82
Koharu-Sato <54939418+Koharu-Sato@users.noreply.github.com>
2019-10-14 16:26:05 +0900

    push

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,6 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @images = Image.new
-
     if @item.save
       redirect_to root_path
     else
