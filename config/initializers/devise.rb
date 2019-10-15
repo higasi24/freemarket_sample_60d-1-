2b6060f6fb470df8170a934b81aed586d70dff0c
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-15 10:49:49 +0900

    facebook認証

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,7 +297,7 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email', callback_url: "#{ENV['HOST']}/users/auth/facebook/callback"
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', info_fields: 'email'
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], scope: %w(email) 
   OmniAuth.config.logger = Rails.logger if Rails.env.development? # debug用
 end
