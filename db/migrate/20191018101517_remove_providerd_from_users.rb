26499523ffcb8d1bf10f199e8c91154285ed9ff5
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-18 19:16:36 +0900

    removeカラム

---
+++ db/migrate/20191018101517_remove_providerd_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveProviderdFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :provider, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_15_055303) do
+ActiveRecord::Schema.define(version: 2019_10_18_101517) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code"
@@ -156,15 +156,11 @@ ActiveRecord::Schema.define(version: 2019_10_15_055303) do
     t.string "first_name_kana", null: false
     t.string "family_name_kana", null: false
     t.string "birth_date", null: false
-    t.string "provider"
-    t.string "uid"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
