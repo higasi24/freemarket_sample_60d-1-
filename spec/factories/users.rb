8cf59f6fe6268cff8dcd2ec12bd579825417bac4
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-19 22:39:55 +0900

    単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,14 @@
 FactoryBot.define do
   factory :user do
     password = Faker::Internet.password(min_length: 7)
-    email {Faker::Internet.free_email}
-    password {password}
+    nickname              {"エキスパ太郎"}
+    email                 {Faker::Internet.free_email}
+    password              {password}
     password_confirmation {password}
+    first_name            {Faker::Name.first_name }
+    first_name_kana       {"ヤマダ"}
+    last_name             {Faker::Name.last_name }
+    last_name_kana        {"シンタロウ"}
+    birth_date            {1=>1990, 2=>1, 3=>1}
   end
 end
