0cab7bf0b5f0680083d1435cbbf1c7ecbdb1e322
ito0804takuya <ito0804takuya@gmail.com>
2019-10-20 12:41:55 +0900

    仮保存

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe ItemsController do
 
-  describe 'GET #show' do
-    it "renders the :show template" do
-      user = create(:user)
-      item = create(:item)
-      get :show, params: { id: 1 }
-      expect(response).to render_template :show
+    describe 'GET #show' do
+      it "renders the :show template" do
+        user = create(:user)
+        item = create(:item)
+        get :show, params: { id: 1 }
+        expect(response).to render_template :show
+      end
     end
-  end
 
 end
