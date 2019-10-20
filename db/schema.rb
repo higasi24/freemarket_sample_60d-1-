9fbd1eaf8089520c67b2424d0001b6ae3cc027ef
tKm1 <tk3rw_falilv@yahoo.co.jp>
2019-10-20 15:45:59 +0900

    update show-box.html.haml

--- db/schema.rb
+++ db/schema.rb
@@ -153,8 +153,6 @@ ActiveRecord::Schema.define(version: 2019_10_20_060631) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "addresses", "users"
