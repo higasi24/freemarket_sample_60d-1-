1dfdfdb056c3ec97d3a47ed38d2c0e1831c5ec4e
higasi <higasi.nba1209@au.com>
2019-10-21 18:15:22 +0900

    商品購入のテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,8 +7,8 @@ FactoryBot.define do
     password_confirmation {password}
     first_name            {Faker::Name.first_name }
     first_name_kana       {"ヤマダ"}
-    last_name             {Faker::Name.last_name }
-    last_name_kana        {"シンタロウ"}
+    # last_name             {Faker::Name.last_name }
+    # last_name_kana        {"シンタロウ"}
     birth_date            {{1=>1990, 2=>1, 3=>1}}
     family_name {"fff"}
     family_name_kana {"カナ"}
